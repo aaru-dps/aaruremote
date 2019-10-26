407078cefecaeec827eed4bdd823a5b15334b5c0
Natalia Portillo <claunia@claunia.com>
2019-10-27 00:14:48 +0100

    Implement AmIRoot for win32.

--- win32/win32.c
+++ win32/win32.c
@@ -26,6 +26,18 @@ void PlatformLoop(DicPacketHello* pkt_server_hello) { WorkingLoop(pkt_server_hel
 
 uint8_t AmIRoot()
 {
-    // TODO: Check administrative privileges
-    return 0;
+    BOOL                     b;
+    SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+    PSID                     AdministratorsGroup;
+
+    b = AllocateAndInitializeSid(
+        &NtAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &AdministratorsGroup);
+
+    if(b)
+    {
+        if(!CheckTokenMembership(NULL, AdministratorsGroup, &b)) { b = FALSE; }
+        FreeSid(AdministratorsGroup);
+    }
+
+    return b;
 }
